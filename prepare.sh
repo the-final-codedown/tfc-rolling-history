6b3a7c0192edb028d94d8262d5c4036b8eb6ecfc
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-02-14 20:16:15 +0100

    Updated prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,9 @@
 
 mvn clean package
 
-docker build -t tfc/rolling-history .
+IMAGE=tfc/rolling-history
+VERSION=
+
+docker build -t ${IMAGE} .
+docker tag ${IMAGE} localhost:5000/${IMAGE}${VERSION}
+docker push localhost:5000/${IMAGE}${VERSION}
